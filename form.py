f992cd581be1ff907051695bc50a3ccee2316814
katelyj <kjohnston@stuy.edu>
2016-10-06 19:14:26 -0400

    there is a login button! it just doesnt do anything yet

--- form.py
+++ form.py
@@ -3,7 +3,7 @@ import hashlib
 import csv
 
 ramirez = Flask(__name__)
-ramirez.secret_key = "fhruvhsfjkghdjhjdf"
+ramirez.secret_key = "\xceY\x96\x0b^\x8d:\xea\xd2\x9c\xae\xf2\xfc\xdd\x8dL\xf5Dsw\x8a\xcb=\xf8\xf1\xe5\x89K\xbd\x17\x1eg"
 
 def hash(s):
     p = hashlib.sha1()
@@ -11,8 +11,8 @@ def hash(s):
     return p.hexdigest()
 
 def register(user,password):
-    data = csv.reader(open("data/d.csv"))
-    for i in data:
+    DATA = csv.reader(open("data/d.csv"))
+    for i in DATA:
 	if user == i[0]:
 		return "Ya registered already, punk!"
     with open('data/d.csv', 'a') as f:
@@ -21,12 +21,12 @@ def register(user,password):
     return "Congrats! You can now log in..."
 
 def check(user,password):
-    data = csv.reader(open("data/d.csv"))
-    for i in data:
+    DATA = csv.reader(open("data/d.csv"))
+    for i in DATA:
 	if user == i[0]:
 	    if i[1] == hash(password):
 		return "Success!"
-	return "You went wrong there, hun."
+	    return "You went wrong there, hun."
     return "Have you even registered?"
 
 @ramirez.route("/")
@@ -35,7 +35,7 @@ def home():
     #print request headers
     print url_for('home')
     print url_for('auth')
-    return render_template("form.html")
+    return render_template("form.html",result="Write whatever comes to your heart...")
 
 @ramirez.route("/authenticate/", methods=['GET','POST'])
 def auth():
@@ -46,7 +46,7 @@ def auth():
 	m = register(user,password)
     else:
         m = check(user,password)
-    return render_template("results.html",result=m)
+    return render_template("form.html",result=m)
 
 if __name__ == '__main__':
     ramirez.debug = True
